6494a7270ad0ca5d0b54a59ac857e6f275a55542
inokappa <inokara@gmail.com>
2013-11-03 14:48:20 +0900

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+fluentd_forwarder_setup CHANGELOG
+=================================
+
+This file is used to list changes made in each version of the fluentd_forwarder_setup cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of fluentd_forwarder_setup
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,76 @@
+#
+# Cookbook Name:: fluentd_forwarder_setup
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute 'apt-get update' do
+  command "apt-get update"
+end
+
+package "nginx" do
+  action :install
+end
+
+directory "/var/www" do
+  owner "root"
+  group "root"
+  mode 00755
+  action :create
+end
+
+template "/var/www/index.html" do
+  source "index.html.erb"
+  owner "root"
+  group "root"
+  mode "00644"
+  not_if {File.exists?("/var/www")}
+end
+
+package "snmpd" do
+  action :install
+end
+
+service "snmpd" do
+  supports :restart => true
+end
+
+template "/etc/snmp/snmpd.conf" do
+  source "snmpd.conf.erb"
+  owner "root"
+  group "root"
+  mode "00644"
+  notifies :restart, "service[snmpd]"
+end
+
+%w{libxslt1.1 libyaml-0-2}.each do |pkgs|
+  package pkgs do
+    action :install
+  end
+end
+
+execute "install_td-agent" do
+  command "dpkg -i /tmp/td-agent_1.1.17-1_amd64.deb"
+  action :nothing
+end
+
+remote_file "/tmp/td-agent_1.1.17-1_amd64.deb" do
+  source "http://packages.treasure-data.com/debian/pool/contrib/t/td-agent/td-agent_1.1.17-1_amd64.deb"
+  notifies :run, "execute[install_td-agent]", :immediately
+  not_if {File.exists?("/etc/init.d/td-agent")}
+end
+
+service "td-agent" do
+  supports :restart => true
+end
+
+template "/etc/td-agent/td-agent.conf" do
+  source "td-agent.conf.erb"
+  owner "root"
+  group "root"
+  mode "00644"
+  notifies :restart, "service[td-agent]"
+end
